538730a8b9cfbb0518114e8bb219cfd9499bdab2
fakeyanss <yanshisangc@gmail.com>
2023-04-06 22:45:03 +0800

    hammerspoon inputsource auto switch

---
+++ software/hammerspoon/Spoons/InputSourceSwitcher.spoon/init.lua
@@ -0,0 +1,85 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "InputSourceSwitcher"
+obj.version = "1.0"
+obj.author = "ibreathebsb"
+obj.github = "https://gist.github.com/ibreathebsb/65fae9d742c5ebdb409960bceaf934de"
+obj.homepage = "https://github.com/Hammerspoon/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+function obj:start()
+	appWatcher = hs.application.watcher.new(applicationWatcher)
+	appWatcher:start()
+end
+
+function obj:stop() end
+
+local function Chinese()
+	-- 简体拼音
+	hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Hans")
+end
+
+local function English()
+	-- ABC
+	hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+end
+
+-- app to expected ime config
+-- app和对应的输入法
+local app2Ime = {
+	{ "/Applications/iTerm.app", "English" },
+	{ "/Applications/Visual Studio Code.app", "English" },
+	{ "/Applications/Google Chrome.app", "English" },
+	{ "/System/Library/CoreServices/Finder.app", "English" },
+	{ "/System/Applications/System Settings.app", "English" },
+	{ "/Applications/如流.app", "Chinese" },
+	{ "/Applications/WeChat.app", "Chinese" },
+}
+
+function updateFocusAppInputMethod()
+	local ime = "English"
+	local focusAppPath = hs.window.frontmostWindow():application():path()
+	for index, app in pairs(app2Ime) do
+		local appPath = app[1]
+		local expectedIme = app[2]
+
+		if focusAppPath == appPath then
+			ime = expectedIme
+			break
+		end
+	end
+
+	if ime == "English" then
+		English()
+	else
+		Chinese()
+	end
+end
+
+-- Handle cursor focus and application's screen manage.
+-- 窗口激活时自动切换输入法
+function applicationWatcher(appName, eventType, appObject)
+	if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.launched then
+		updateFocusAppInputMethod()
+	end
+end
+
+-- helper hotkey to figure out the app path and name of current focused window
+-- 当选中某窗口按下ctrl+command+.时会显示应用的路径等信息
+hs.hotkey.bind({ "ctrl", "option" }, ".", function()
+	hs.pasteboard.setContents(hs.window.focusedWindow():application():path())
+	hs.alert.show(
+		"App path:        "
+			.. hs.window.focusedWindow():application():path()
+			.. "\n"
+			.. "App name:      "
+			.. hs.window.focusedWindow():application():name()
+			.. "\n"
+			.. "IM source id:  "
+			.. hs.keycodes.currentSourceID()
+	)
+end)
+
+return obj
--- software/hammerspoon/init.lua
+++ software/hammerspoon/init.lua
@@ -1,25 +1,26 @@
 -- luacheck: globals hs spoon
 -- define spoons and config, which will be passed to :start
 local spoons = {
-    -- ModalMgr Spoon must be loaded explicitly, because HSaria2 relies upon it.
-    ModalMgr = {},
-    HoldToQuit = {},
-    SpeedMenu = {},
-    HSaria2 = {},
-    Caffeine = {},
-    ReloadConfiguration = {},
-    -- UnsplashZ = {},
-    WinWin = {},
-    -- Clipboard = {}, -- raycast clipboard is better
-    -- EjectMenu = {},
-    TimeFlow = {},
-    -- Karabiner = {},
+	-- ModalMgr Spoon must be loaded explicitly, because HSaria2 relies upon it.
+	ModalMgr = {},
+	HoldToQuit = {},
+	SpeedMenu = {},
+	HSaria2 = {},
+	Caffeine = {},
+	ReloadConfiguration = {},
+	-- UnsplashZ = {},
+	WinWin = {},
+	-- Clipboard = {}, -- raycast clipboard is better
+	-- EjectMenu = {},
+	TimeFlow = {},
+	-- Karabiner = {},
+	InputSourceSwitcher = {},
 }
 
 -- load spoons
 for spoonName, spoonConfig in pairs(spoons) do
-    hs.loadSpoon(spoonName)
-    spoon[spoonName]:start(spoonConfig)
+	hs.loadSpoon(spoonName)
+	spoon[spoonName]:start(spoonConfig)
 end
 
 -- load vim mode
@@ -33,66 +34,231 @@ end
 ----------------------------------------------------------------------------------------------------
 -- resizeM modal environment
 if spoon.WinWin then
-    spoon.ModalMgr:new("resizeM")
-    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
-    -- using escape will make esc not working in Mac OS Monterey, and reboot can fix it
-    -- cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
-    cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
-    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
-    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
-    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
-    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
-    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
-    cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
-    cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
-    cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
-    cmodal:bind('', 'J', 'Downhalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
-    cmodal:bind('', 'Y', 'NorthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
-    cmodal:bind('', 'O', 'NorthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
-    cmodal:bind('', 'U', 'SouthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
-    cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
-    cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
-    cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
-    cmodal:bind('', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
-    cmodal:bind('', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
-    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
-    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
-    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
-    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
-    cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
-    cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
-    cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
-    cmodal:bind('', 'down', 'Move to Below Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
-    cmodal:bind('', 'space', 'Move to Next Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
-    cmodal:bind('', '[', 'Undo Window Manipulation', function() spoon.WinWin:undo() end)
-    cmodal:bind('', ']', 'Redo Window Manipulation', function() spoon.WinWin:redo() end)
-    cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
+	spoon.ModalMgr:new("resizeM")
+	local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+	-- using escape will make esc not working in Mac OS Monterey, and reboot can fix it
+	-- cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+	cmodal:bind("", "Q", "Deactivate resizeM", function()
+		spoon.ModalMgr:deactivate({ "resizeM" })
+	end)
+	cmodal:bind("", "tab", "Toggle Cheatsheet", function()
+		spoon.ModalMgr:toggleCheatsheet()
+	end)
+	cmodal:bind(
+		"",
+		"A",
+		"Move Leftward",
+		function()
+			spoon.WinWin:stepMove("left")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepMove("left")
+		end
+	)
+	cmodal:bind(
+		"",
+		"D",
+		"Move Rightward",
+		function()
+			spoon.WinWin:stepMove("right")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepMove("right")
+		end
+	)
+	cmodal:bind(
+		"",
+		"W",
+		"Move Upward",
+		function()
+			spoon.WinWin:stepMove("up")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepMove("up")
+		end
+	)
+	cmodal:bind(
+		"",
+		"S",
+		"Move Downward",
+		function()
+			spoon.WinWin:stepMove("down")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepMove("down")
+		end
+	)
+	cmodal:bind("", "H", "Lefthalf of Screen", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("halfleft")
+	end)
+	cmodal:bind("", "L", "Righthalf of Screen", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("halfright")
+	end)
+	cmodal:bind("", "K", "Uphalf of Screen", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("halfup")
+	end)
+	cmodal:bind("", "J", "Downhalf of Screen", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("halfdown")
+	end)
+	cmodal:bind("", "Y", "NorthWest Corner", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("cornerNW")
+	end)
+	cmodal:bind("", "O", "NorthEast Corner", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("cornerNE")
+	end)
+	cmodal:bind("", "U", "SouthWest Corner", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("cornerSW")
+	end)
+	cmodal:bind("", "I", "SouthEast Corner", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("cornerSE")
+	end)
+	cmodal:bind("", "F", "Fullscreen", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("fullscreen")
+	end)
+	cmodal:bind("", "C", "Center Window", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveAndResize("center")
+	end)
+	cmodal:bind(
+		"",
+		"=",
+		"Stretch Outward",
+		function()
+			spoon.WinWin:moveAndResize("expand")
+		end,
+		nil,
+		function()
+			spoon.WinWin:moveAndResize("expand")
+		end
+	)
+	cmodal:bind(
+		"",
+		"-",
+		"Shrink Inward",
+		function()
+			spoon.WinWin:moveAndResize("shrink")
+		end,
+		nil,
+		function()
+			spoon.WinWin:moveAndResize("shrink")
+		end
+	)
+	cmodal:bind(
+		"shift",
+		"H",
+		"Move Leftward",
+		function()
+			spoon.WinWin:stepResize("left")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepResize("left")
+		end
+	)
+	cmodal:bind(
+		"shift",
+		"L",
+		"Move Rightward",
+		function()
+			spoon.WinWin:stepResize("right")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepResize("right")
+		end
+	)
+	cmodal:bind(
+		"shift",
+		"K",
+		"Move Upward",
+		function()
+			spoon.WinWin:stepResize("up")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepResize("up")
+		end
+	)
+	cmodal:bind(
+		"shift",
+		"J",
+		"Move Downward",
+		function()
+			spoon.WinWin:stepResize("down")
+		end,
+		nil,
+		function()
+			spoon.WinWin:stepResize("down")
+		end
+	)
+	cmodal:bind("", "left", "Move to Left Monitor", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveToScreen("left")
+	end)
+	cmodal:bind("", "right", "Move to Right Monitor", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveToScreen("right")
+	end)
+	cmodal:bind("", "up", "Move to Above Monitor", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveToScreen("up")
+	end)
+	cmodal:bind("", "down", "Move to Below Monitor", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveToScreen("down")
+	end)
+	cmodal:bind("", "space", "Move to Next Monitor", function()
+		spoon.WinWin:stash()
+		spoon.WinWin:moveToScreen("next")
+	end)
+	cmodal:bind("", "[", "Undo Window Manipulation", function()
+		spoon.WinWin:undo()
+	end)
+	cmodal:bind("", "]", "Redo Window Manipulation", function()
+		spoon.WinWin:redo()
+	end)
+	cmodal:bind("", "`", "Center Cursor", function()
+		spoon.WinWin:centerCursor()
+	end)
 
-    -- Register resizeM with modal supervisor
-    hsresizeM_keys = hsresizeM_keys or {"alt", "R"}
-    if string.len(hsresizeM_keys[2]) > 0 then
-        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
-            -- Deactivate some modal environments or not before activating a new one
-            spoon.ModalMgr:deactivateAll()
-            -- Show an status indicator so we know we're in some modal environment now
-            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
-        end)
-    end
+	-- Register resizeM with modal supervisor
+	hsresizeM_keys = hsresizeM_keys or { "alt", "R" }
+	if string.len(hsresizeM_keys[2]) > 0 then
+		spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
+			-- Deactivate some modal environments or not before activating a new one
+			spoon.ModalMgr:deactivateAll()
+			-- Show an status indicator so we know we're in some modal environment now
+			spoon.ModalMgr:activate({ "resizeM" }, "#B22222")
+		end)
+	end
 end
 
 -- Register HSaria2
 if spoon.HSaria2 then
-    -- First we need to connect to aria2 rpc host
-    hsaria2_host = "http://localhost:6800/jsonrpc"
-    hsaria2_secret = "fakeyanss"
-    spoon.HSaria2:connectToHost(hsaria2_host, hsaria2_secret)
+	-- First we need to connect to aria2 rpc host
+	hsaria2_host = "http://localhost:6800/jsonrpc"
+	hsaria2_secret = "fakeyanss"
+	spoon.HSaria2:connectToHost(hsaria2_host, hsaria2_secret)
 
-    hsaria2_keys = hsaria2_keys or {"alt", "D"}
-    if string.len(hsaria2_keys[2]) > 0 then
-        spoon.ModalMgr.supervisor:bind(hsaria2_keys[1], hsaria2_keys[2], 'Toggle aria2 Panel', function()
-            spoon.HSaria2:togglePanel()
-        end)
-    end
+	hsaria2_keys = hsaria2_keys or { "alt", "D" }
+	if string.len(hsaria2_keys[2]) > 0 then
+		spoon.ModalMgr.supervisor:bind(hsaria2_keys[1], hsaria2_keys[2], "Toggle aria2 Panel", function()
+			spoon.HSaria2:togglePanel()
+		end)
+	end
 end
 
 ----------------------------------------------------------------------------------------------------
